2226853d25ca30ff14263d913ad74d98336890c0
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2023-06-21 13:23:26 +0300

    Updated comment of init.bash and fixed quotes

--- snaker/init.bash
+++ snaker/init.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# The Main File
+# The Main File of Snaker. It loads every needed module.
 
 # shellcheck disable=SC1091
 
 source "/usr/lib/snaker/env.bash";
-source "$SNAKER_PATH"/loader.bash;
-source "$SNAKER_PATH"/functions.bash;
+source "$SNAKER_PATH/loader.bash";
+source "$SNAKER_PATH/functions.bash";
